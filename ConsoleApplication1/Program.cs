01c04cee73d11dadec2831a088b64571f1b3399d
zloihach <72695485+zloihach@users.noreply.github.com>
2022-11-30 19:45:11 +0300

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using static ClassLibrary1.MyStopwatch;
+
+namespace StopWatchSample
+{
+    class OperationsTimer
+    {
+        public static void Main()
+        {
+            DisplayTimerProperties();
+
+            Console.WriteLine();
+            Console.WriteLine("Press the Enter key to begin:");
+            Console.ReadLine();
+            Console.WriteLine();
+            TimeOperations();
+        }
+    }
+}
